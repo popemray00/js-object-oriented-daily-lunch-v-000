bcb4e8b1459dfef98bbc335d5f06b8a8388ba07b
Michael Pope <popemray1986@gmail.com>
2018-06-20 00:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ let neighborhoodId = 0
 let customerId = 0
 let mealId = 0
 let deliveryId = 0
+
+class Neighborhood {
+  constructor(name) {
+    this.neighborhoodId = ++neighborhoodId 
+    
+  }
+}
